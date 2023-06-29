e83c11cfdc078e8a82094acdb384e52c7df325ed
Mateusz Majchrzak <36929127+maaajo@users.noreply.github.com>
2023-06-29 11:57:02 +0200

    Adding social buttons

---
+++ crm-demo-app/src/components/email-input.tsx
@@ -0,0 +1,15 @@
+import { InputGroup, Input, InputLeftElement, Icon } from "@chakra-ui/react";
+import { AtSign } from "lucide-react";
+
+const EmailInput = () => {
+  return (
+    <InputGroup size={"md"}>
+      <InputLeftElement pointerEvents={"none"}>
+        <Icon as={AtSign} color={"gray.500"} />
+      </InputLeftElement>
+      <Input type={"email"} placeholder="Enter Password" />
+    </InputGroup>
+  );
+};
+
+export default EmailInput;
--- crm-demo-app/src/pages/auth/sign-up.tsx
+++ crm-demo-app/src/pages/auth/sign-up.tsx
@@ -5,21 +5,64 @@ import {
   FormLabel,
   Input,
   VStack,
+  Button,
+  AbsoluteCenter,
+  Divider,
+  HStack,
 } from "@chakra-ui/react";
+import EmailInput from "@/components/email-input";
 import PasswordInput from "@/components/password-input";
 
 const SignUp = () => {
   return (
-    <Flex width={"full"} justifyContent={"center"} alignItems={"center"}>
-      <VStack spacing={"2"} width={"md"}>
+    <Flex
+      width={"full"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      direction={"column"}
+    >
+      <VStack spacing={"4"} width={"md"}>
         <FormControl>
-          <FormLabel>Email Address</FormLabel>
-          <Input type={"email"} />
+          <FormLabel fontWeight={"bold"}>Email Address</FormLabel>
+          <EmailInput />
         </FormControl>
         <FormControl>
-          <FormLabel>Password</FormLabel>
+          <FormLabel fontWeight={"bold"}>Password</FormLabel>
           <PasswordInput />
         </FormControl>
+        <Button
+          variant={"outline"}
+          colorScheme={"blackAlpha"}
+          color={"blackAlpha.900"}
+          width={"full"}
+          type={"submit"}
+        >
+          Create Account
+        </Button>
+      </VStack>
+      <Box position={"relative"} py={8} w={"md"}>
+        <Divider borderColor={"blackAlpha.700"} variant={"solid"} />
+        <AbsoluteCenter fontWeight={"bold"} bg="white" px="6">
+          Or
+        </AbsoluteCenter>
+      </Box>
+      <VStack width={"md"} spacing={4}>
+        <Button
+          width={"full"}
+          variant={"outline"}
+          colorScheme={"blackAlpha"}
+          color={"blackAlpha.900"}
+        >
+          Create account with Google
+        </Button>
+        <Button
+          width={"full"}
+          variant={"outline"}
+          colorScheme={"blackAlpha"}
+          color={"blackAlpha.900"}
+        >
+          Create account with Github
+        </Button>
       </VStack>
     </Flex>
   );

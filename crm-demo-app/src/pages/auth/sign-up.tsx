b575fb9f36e009da613e6337d7c15f9152063177
Mateusz Majchrzak <36929127+maaajo@users.noreply.github.com>
2023-07-11 09:15:05 +0200

    added server side checks and disabled buttons

--- crm-demo-app/src/pages/auth/sign-up.tsx
+++ crm-demo-app/src/pages/auth/sign-up.tsx
@@ -13,15 +13,14 @@ import {
 import EmailInput from "@/components/email-input";
 import PasswordInput from "@/components/password-input";
 import { Google, Github } from "grommet-icons";
-import {
-  useSupabaseClient,
-  useSessionContext,
-} from "@supabase/auth-helpers-react";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import validator from "validator";
 import { useRouter } from "next/router";
+import { NextPage, GetServerSideProps } from "next";
 
 const signUpSchema = z.object({
   email: z
@@ -39,16 +38,15 @@ const signUpSchema = z.object({
 
 type SignUpSchema = z.infer<typeof signUpSchema>;
 
-const SignUp = () => {
+const SignUp: NextPage = () => {
   const toast = useToast();
   const supabase = useSupabaseClient();
   const router = useRouter();
-  const { isLoading, session, error } = useSessionContext();
 
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm<SignUpSchema>({
     resolver: zodResolver(signUpSchema),
   });
@@ -57,7 +55,6 @@ const SignUp = () => {
   const { ref: formPasswordRef, ...passwordRest } = register("password");
 
   const onSubmit: SubmitHandler<SignUpSchema> = async (signUpData) => {
-    console.log(signUpData);
     const { data, error } = await supabase.auth.signUp({
       email: signUpData.email,
       password: signUpData.password,
@@ -65,12 +62,12 @@ const SignUp = () => {
     });
 
     if (data.session) {
-      router.push("/");
+      router.reload();
     }
 
     if (error) {
       toast({
-        title: "Ooops, something went wrong!",
+        title: "Failed to register",
         description: error.message,
         status: "error",
         isClosable: true,
@@ -80,96 +77,113 @@ const SignUp = () => {
     }
   };
 
-  if (isLoading) {
-    return <p>Loading</p>;
-  }
+  return (
+    <chakra.form
+      width={"full"}
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      flexDirection={"column"}
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <VStack spacing={"4"} width={"md"}>
+        <FormControl isInvalid={Boolean(errors.email)}>
+          <FormLabel fontWeight={"bold"}>Email Address</FormLabel>
+          <EmailInput
+            {...emailRest}
+            name={"email"}
+            emailRef={(e) => {
+              formEmailRef(e);
+            }}
+            isDisabled={isSubmitting}
+          />
+          {errors.email && (
+            <FormErrorMessage>
+              {errors.email.message!.toString()}
+            </FormErrorMessage>
+          )}
+        </FormControl>
+        <FormControl isInvalid={Boolean(errors.password)}>
+          <FormLabel fontWeight={"bold"}>Password</FormLabel>
+          <PasswordInput
+            {...passwordRest}
+            name={"password"}
+            passwordRef={(e) => {
+              formPasswordRef(e);
+            }}
+            isDisabled={isSubmitting}
+          />
+          {errors.password && (
+            <FormErrorMessage>
+              {errors.password.message!.toString()}
+            </FormErrorMessage>
+          )}
+        </FormControl>
+        <Button
+          variant={"solid"}
+          width={"full"}
+          type={"submit"}
+          bgColor={"black"}
+          color={"white"}
+          _hover={{
+            bgColor: "blackAlpha.800",
+          }}
+          isLoading={isSubmitting}
+          loadingText={"Signing up..."}
+        >
+          Create Account
+        </Button>
+      </VStack>
+      <Box position={"relative"} py={8} w={"md"}>
+        <Divider borderColor={"blackAlpha.700"} variant={"solid"} />
+        <AbsoluteCenter fontWeight={"bold"} bg="white" px="6">
+          Or
+        </AbsoluteCenter>
+      </Box>
+      <VStack width={"md"} spacing={4}>
+        <Button
+          leftIcon={<Google color={"brand"} />}
+          width={"full"}
+          variant={"outline"}
+          colorScheme={"blackAlpha"}
+          color={"blackAlpha.900"}
+          isLoading={isSubmitting}
+        >
+          Sign in with Google
+        </Button>
+        <Button
+          leftIcon={<Github color={"brand"} />}
+          width={"full"}
+          variant={"outline"}
+          colorScheme={"blackAlpha"}
+          color={"blackAlpha.900"}
+          isLoading={isSubmitting}
+        >
+          Sign in with Github
+        </Button>
+      </VStack>
+    </chakra.form>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const supabase = createServerSupabaseClient(ctx);
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
 
   if (session) {
-    console.log("Session:", session);
-    router.push("/");
-  } else {
-    return (
-      <chakra.form
-        width={"full"}
-        display={"flex"}
-        justifyContent={"center"}
-        alignItems={"center"}
-        flexDirection={"column"}
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <VStack spacing={"4"} width={"md"}>
-          <FormControl isInvalid={Boolean(errors.email)}>
-            <FormLabel fontWeight={"bold"}>Email Address</FormLabel>
-            <EmailInput
-              {...emailRest}
-              name={"email"}
-              emailRef={(e) => {
-                formEmailRef(e);
-              }}
-            />
-            {errors.email && (
-              <FormErrorMessage>
-                {errors.email.message!.toString()}
-              </FormErrorMessage>
-            )}
-          </FormControl>
-          <FormControl isInvalid={Boolean(errors.password)}>
-            <FormLabel fontWeight={"bold"}>Password</FormLabel>
-            <PasswordInput
-              {...passwordRest}
-              name={"password"}
-              passwordRef={(e) => {
-                formPasswordRef(e);
-              }}
-            />
-            {errors.password && (
-              <FormErrorMessage>
-                {errors.password.message!.toString()}
-              </FormErrorMessage>
-            )}
-          </FormControl>
-          <Button
-            variant={"solid"}
-            width={"full"}
-            type={"submit"}
-            bgColor={"black"}
-            color={"white"}
-            _hover={{
-              bgColor: "blackAlpha.800",
-            }}
-          >
-            Create Account
-          </Button>
-        </VStack>
-        <Box position={"relative"} py={8} w={"md"}>
-          <Divider borderColor={"blackAlpha.700"} variant={"solid"} />
-          <AbsoluteCenter fontWeight={"bold"} bg="white" px="6">
-            Or
-          </AbsoluteCenter>
-        </Box>
-        <VStack width={"md"} spacing={4}>
-          <Button
-            leftIcon={<Google color={"brand"} />}
-            width={"full"}
-            variant={"outline"}
-            colorScheme={"blackAlpha"}
-            color={"blackAlpha.900"}
-          >
-            Sign in with Google
-          </Button>
-          <Button
-            leftIcon={<Github color={"brand"} />}
-            width={"full"}
-            variant={"outline"}
-            colorScheme={"blackAlpha"}
-            color={"blackAlpha.900"}
-          >
-            Sign in with Github
-          </Button>
-        </VStack>
-      </chakra.form>
-    );
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
   }
+
+  return {
+    props: {},
+  };
 };
 
 export default SignUp;

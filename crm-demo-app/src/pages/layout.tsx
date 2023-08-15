84e1d29c58df65c94c73db6a8945b2df076f1d11
Mateusz Majchrzak <matemajch@gmail.com>
2023-08-15 13:29:42 +0200

    changes to sidebar

--- crm-demo-app/src/pages/layout.tsx
+++ crm-demo-app/src/pages/layout.tsx
@@ -1,29 +1,54 @@
 import { ReactNode } from "react";
-import { Box, Text, Flex, Container } from "@chakra-ui/react";
+import { Box, Text, Flex } from "@chakra-ui/react";
+import { useUser } from "@supabase/auth-helpers-react";
 
 interface LayoutProps {
   children?: ReactNode;
 }
+
+const Sidebar = () => {
+  const user = useUser();
+
+  //fix height for body
+
+  return (
+    <Box
+      color={"gray.50"}
+      bg={"gray.800"}
+      w={64}
+      pos="fixed"
+      h="full"
+      boxShadow={"xl"}
+    >
+      <Text
+        fontSize="2xl"
+        fontWeight="extrabold"
+        py={"8"}
+        letterSpacing={"tight"}
+        textAlign={"center"}
+        as={"h3"}
+      >
+        CRM Playground
+      </Text>
+      <Flex
+        height={"83%"}
+        px={"5"}
+        flexDirection={"column"}
+        justifyContent={"space-between"}
+      >
+        <Flex>body</Flex>
+        <Flex>{user?.email}</Flex>
+      </Flex>
+    </Box>
+  );
+};
 
 const Layout = (props: LayoutProps) => {
   const { children } = props;
 
   return (
     <Flex>
-      <Box
-        bg={"white"}
-        borderRight="1px"
-        borderRightColor={"gray.200"}
-        w={{ base: "full", md: "64" }}
-        pos="fixed"
-        h="full"
-      >
-        <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
-          <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
-            Logo
-          </Text>
-        </Flex>
-      </Box>
+      <Sidebar />
       <Flex ml={"64"} pl={"4"}>
         {children}
       </Flex>

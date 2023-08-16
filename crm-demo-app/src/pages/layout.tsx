3362094ffcc0d70ef217da0b4b8dc8399764b64a
Mateusz Majchrzak <matemajch@gmail.com>
2023-08-16 19:48:30 +0200

    fixed sidebar height issue and added log out button

--- crm-demo-app/src/pages/layout.tsx
+++ crm-demo-app/src/pages/layout.tsx
@@ -1,6 +1,9 @@
 import { ReactNode } from "react";
-import { Box, Text, Flex } from "@chakra-ui/react";
+import { Box, Text, Flex, IconButton } from "@chakra-ui/react";
 import { useUser } from "@supabase/auth-helpers-react";
+import { LogOut } from "lucide-react";
+import { Link } from "@chakra-ui/next-js";
+import { routes } from "@/lib/routes";
 
 interface LayoutProps {
   children?: ReactNode;
@@ -9,16 +12,16 @@ interface LayoutProps {
 const Sidebar = () => {
   const user = useUser();
 
-  //fix height for body
-
   return (
     <Box
       color={"gray.50"}
       bg={"gray.800"}
       w={64}
       pos="fixed"
-      h="full"
+      h="100vh"
       boxShadow={"xl"}
+      display={"flex"}
+      flexDirection={"column"}
     >
       <Text
         fontSize="2xl"
@@ -31,13 +34,23 @@ const Sidebar = () => {
         CRM Playground
       </Text>
       <Flex
-        height={"83%"}
+        flex={1}
         px={"5"}
         flexDirection={"column"}
         justifyContent={"space-between"}
       >
-        <Flex>body</Flex>
-        <Flex>{user?.email}</Flex>
+        <Flex flex={9}>body</Flex>
+        <Flex flex={1} flexDirection={"column"}>
+          <Text>{user?.email}</Text>
+          <IconButton
+            aria-label="Log Out"
+            as={Link}
+            href={routes.auth.signOut}
+            icon={<LogOut />}
+            variant={"ghost"}
+            colorScheme={"whiteAlpha"}
+          />
+        </Flex>
       </Flex>
     </Box>
   );

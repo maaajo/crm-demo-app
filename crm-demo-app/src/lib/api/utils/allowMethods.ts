85cce69d4e11940bfae06b8fb6e7800049ede7dc
Mateusz Majchrzak <matemajch@gmail.com>
2023-11-12 11:59:21 +0100

    added allowed methods api handler

--- crm-demo-app/src/lib/api/utils/allowMethods.ts
+++ crm-demo-app/src/lib/api/utils/allowMethods.ts
@@ -1,1 +1,16 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { ApiMiddleware, NextFunction } from "./customHandler";
+
+type HTTPMethod = "GET" | "POST" | "PATCH" | "DELETE" | "PUT";
+
+export const allowMethods =
+  (allowedMethods: HTTPMethod[]): ApiMiddleware =>
+  async (req: NextApiRequest, res: NextApiResponse, next: NextFunction) => {
+    if (allowedMethods.includes(req.method as HTTPMethod)) {
+      return next();
+    } else {
+      res.status(405).send({
+        error: `Endpoint doesn't support requests of type ${req.method}`,
+      });
+    }
+  };

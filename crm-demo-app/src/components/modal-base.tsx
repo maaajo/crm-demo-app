31474477c9ed70a68db34c83f67e144787e36006
Mateusz Majchrzak <matemajch@gmail.com>
2023-11-25 19:14:12 +0100

    added modal base

---
+++ crm-demo-app/src/components/modal-base.tsx
@@ -0,0 +1,54 @@
+import { ReactNode, FormEvent } from "react";
+import {
+  Button,
+  Heading,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalOverlay,
+  HStack,
+  ModalCloseButton,
+  useToast,
+  chakra,
+} from "@chakra-ui/react";
+
+type ModalBaseProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  isSubmitting: boolean;
+  title: string;
+  children: ReactNode;
+};
+
+const ModalBase = ({
+  isOpen,
+  onClose,
+  isSubmitting,
+  title,
+  children,
+}: ModalBaseProps) => {
+  return (
+    <Modal
+      closeOnOverlayClick={!isSubmitting}
+      isOpen={isOpen}
+      onClose={onClose}
+      isCentered
+    >
+      <ModalOverlay
+        bg="blackAlpha.300"
+        backdropFilter="blur(10px) hue-rotate(90deg)"
+      />
+      <ModalContent py={8} px={4}>
+        <ModalCloseButton isDisabled={isSubmitting} />
+        <Heading fontSize={"2xl"} fontWeight={"extrabold"}>
+          {title}
+        </Heading>
+        <ModalBody textAlign={"center"} mt={10} mb={4}>
+          {children}
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default ModalBase;
